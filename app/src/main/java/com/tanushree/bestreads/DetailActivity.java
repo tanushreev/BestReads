c0472105b00b73184876bc6090184d5b20113208
Tanushree Varshney <varshney.tanushree@gmail.com>
2019-12-19 21:12:34 +0530

    Create the layout for DetailActivity

--- app/src/main/java/com/tanushree/bestreads/DetailActivity.java
+++ app/src/main/java/com/tanushree/bestreads/DetailActivity.java
@@ -1,14 +1,22 @@
 package com.tanushree.bestreads;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
 public class DetailActivity extends AppCompatActivity {
 
+    private Toolbar mToolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        mToolbar = findViewById(R.id.toolbarDetailActivity);
+
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
