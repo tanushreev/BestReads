06d1adcae087d5a949eb3dfea5ed498b99562bb6
Tanushree Varshney <varshney.tanushree@gmail.com>
2019-12-22 17:05:11 +0530

    Use Google AdMob to display ad in DetailActivity

--- app/src/main/java/com/tanushree/bestreads/DetailActivity.java
+++ app/src/main/java/com/tanushree/bestreads/DetailActivity.java
@@ -15,6 +15,11 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
 import com.squareup.picasso.Picasso;
 import com.tanushree.bestreads.model.AppDatabase;
 import com.tanushree.bestreads.model.AppExecutors;
@@ -33,6 +38,7 @@ public class DetailActivity extends AppCompatActivity {
     private TextView mAuthorTv;
     private TextView mDescriptionTv;
     private Button mAmazonButton;
+    private AdView mAdView;
     private Toolbar mToolbar;
 
     // Member variable for the database.
@@ -53,10 +59,18 @@ public class DetailActivity extends AppCompatActivity {
         mAuthorTv = findViewById(R.id.tvAuthor);
         mDescriptionTv = findViewById(R.id.tvDescription);
         mAmazonButton = findViewById(R.id.bAmazon);
+        mAdView = findViewById(R.id.adView);
         mToolbar = findViewById(R.id.toolbarDetailActivity);
 
         setSupportActionBar(mToolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // Initialize Mobile Ads SDK
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+            }
+        });
 
         mDatabase = AppDatabase.getInstance(getApplicationContext());
 
@@ -172,6 +186,10 @@ public class DetailActivity extends AppCompatActivity {
         }
         else
             mDescriptionTv.setText(mBook.getDescription());
+
+        // Ad
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
     }
 
 }
